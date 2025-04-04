78c58e8c0bd77e5b088508ce4cc5e0c4e3228b3d
PChris04 <paulkruger613@gmail.com>
2025-04-04 20:14:23 +0200

    Updated site

--- portfolio/src/pages/home.js
+++ portfolio/src/pages/home.js
@@ -1,6 +1,7 @@
 export default function Home() {
-    return (
-      <div className="home">
+
+    return(
+        <div className="home">
         <header>
           <h1>Paul Kruger</h1>
           <p>Penetration Tester | Ethical Hacker | Cybersecurity Enthusiast</p>
@@ -18,14 +19,13 @@ export default function Home() {
         <li>Bug Bounty Hunting</li>
         <li>Security Research & Exploit Development</li>
     </ul>
-
+  
     <p>Beyond technical skills, I am an active leader and mentor in cybersecurity communities. I have participated in multiple hackathons and competitions, demonstrating problem-solving skills under pressure. My goal is to continuously expand my knowledge while helping others secure their digital assets.</p>
-
+  
     <p>When I'm not securing systems, I enjoy exploring human psychology, leadership, and personal development. I believe that cybersecurity is not just about technology but also about understanding human behavior to predict and prevent cyber threats.</p>
-</section>
+  </section>
         
       </div>
-
     );
-  }
-  
+
+}
